a17cb0528e520ea22b9dad08c2bcd66ca7e7e6b3
Ubuntu <ubuntu@ip-172-26-8-154.eu-west-2.compute.internal>
2023-11-07 19:21:32 +0000

    Fix 'new error' bug

--- services/logger.js
+++ services/logger.js
@@ -5,7 +5,7 @@ exports.logEvent = async (event) => {
     //If a hash has been provided to us, see if we can identify the user
     //so we can store their name in the log message
     if (event.type === undefined || event.name === undefined) {
-        throw new Error("Name and Type must be defined at minimum");
+        throw Error("Name and Type must be defined at minimum");
     }
 
     if (event.hash !== undefined) {
@@ -24,7 +24,7 @@ exports.logEvent = async (event) => {
 
 exports.getEvents = async(startnum, endnum, device) => {
     if (isNaN(startnum)|| isNaN(endnum) || startnum < 0 || endnum < 0 || endnum < startnum) {
-        throw new Error("Invalid start/end numbers")
+        throw Error("Invalid start/end numbers")
     }
     return await LogEventModel.find(startnum, endnum, device);
 }
