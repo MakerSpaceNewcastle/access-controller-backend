401eedda7a3b368bb41eb4a6de036c8fc42551f0
David Pye <davidmpye@gmail.com>
2023-11-07 16:14:26 +0000

    Update to log service to capture the user name in the message if the card hash is known

--- services/logger.js
+++ services/logger.js
@@ -2,6 +2,18 @@ const e = require('express');
 const LogEventModel = require('../models/logevent');
 
 exports.logEvent = async (event) => {
+    //If a hash has been provided to us, see if we can identify the user
+    //so we can store their name in the log message
+    if (event.hash !== undefined) {
+        //Look up the user this hash is registered to, and set message.
+        let user = await RfidModel.findUserByHash(event.hash);
+        if (user !== undefined) {
+            event.message = user.name + " (" + event.hash + ")";
+        }
+        else {
+            event.message = "Unknown - " + event.hash;
+        }
+    }
     return await LogEventModel.create(event);
 }
 
